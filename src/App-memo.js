b6386ededcc347301f8919d700d473841a7c7182
AbdelrahmanEhab <abdoehab5008@gmail.com>
2024-11-19 00:20:30 +0200

    testing and trying use Memo

---
+++ src/App-memo.js
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
+import Archive from "./components/Archive";
+import { PostProvider } from "./context/PostContext";
+
+function AppMemo() {
+  const [isFakeDark, setIsFakeDark] = useState(false);
+  const [show, setIsSHow] = useState(false);
+
+  // Whenever `isFakeDark` changes, we toggle the `fake-dark-mode` class on the HTML element (see in "Elements" dev tool).
+  useEffect(
+    function () {
+      document.documentElement.classList.toggle("fake-dark-mode");
+    },
+    [isFakeDark]
+  );
+
+  return (
+    // adding context provider + object of the value for state and functions
+    // 2) second step context
+    // ! 2.1 post list ---> searchedPosts ,
+    // * 2.2 adding Function ---> handleAddPost ,
+    // ? 2.3 clearing Function ---> handleClearPosts
+    // ! 2.4 Search Query ,
+    // * 2.5 Set Search Query
+
+    <section>
+      <button
+        onClick={() => setIsFakeDark((isFakeDark) => !isFakeDark)}
+        className="btn-fake-dark-mode"
+      >
+        {isFakeDark ? "☀️" : "🌙"}
+      </button>
+
+      <PostProvider>
+        <Header />
+        <Main />
+        <Archive show={false} />
+        <Footer />
+      </PostProvider>
+    </section>
+  );
+}
+
+export default AppMemo;
--- src/components/List.js
+++ src/components/List.js
@@ -1,17 +1,21 @@
 import { usePosts } from "../context/PostContext";
+import Test from "../Test";
 
 function List() {
   // ! access the context
   const { posts } = usePosts();
   return (
-    <ul>
-      {posts.map((post, index) => (
-        <li key={index}>
-          <h3>{post.title}</h3>
-          <p>{post.body}</p>
-        </li>
-      ))}
-    </ul>
+    <>
+      <ul>
+        {posts.map((post, index) => (
+          <li key={index}>
+            <h3>{post.title}</h3>
+            <p>{post.body}</p>
+          </li>
+        ))}
+      </ul>
+      {/* <Test /> */}
+    </>
   );
 }
 
